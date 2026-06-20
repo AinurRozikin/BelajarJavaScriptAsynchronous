b66b19f6f3d0e818432e937716f141b665cdf90c
Ainur Rozikin <rozikinainur742@gmail.com>
2026-06-20 19:53:43 +0700

    merubah warna halaman menjadi merah, kuning, hijau, biru

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
+// set timeout sederhana dengan console log
 console.log('Kode ini berjalan sebelum set timeout');
 setTimeout(() => {
     return console.log('Kode ini berjalan di dalam set timeout')
     }, 10000);
 console.log('Kode ini berjalan setelah set timeout');
+
+// set timeout merubah warna halaman
+setTimeout(() => {
+    document.body.style.backgroundColor = 'red';
+        setTimeout(() => {
+        document.body.style.backgroundColor = 'yellow';
+            setTimeout(() => {
+            document.body.style.backgroundColor = 'green';
+                setTimeout(() => {
+                document.body.style.backgroundColor = 'blue';
+                }, 1000);
+            }, 1000);
+    }, 1000);
+}, 1000);
