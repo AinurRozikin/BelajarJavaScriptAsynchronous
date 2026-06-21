a40a179c9f7f061f033033a22ec9fecab82669e8
Ainur Rozikin <rozikinainur742@gmail.com>
2026-06-21 10:38:30 +0700

    menggunakan promise untuk menghindari callback hell

--- main.js
+++ main.js
@@ -5,16 +5,31 @@ setTimeout(() => {
     }, 10000);
 console.log('Kode ini berjalan setelah set timeout');
 
-// set timeout merubah warna halaman
-setTimeout(() => {
-    document.body.style.backgroundColor = 'red';
+// set timeout merubah warna halaman atau callback hell
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//         setTimeout(() => {
+//         document.body.style.backgroundColor = 'yellow';
+//             setTimeout(() => {
+//             document.body.style.backgroundColor = 'green';
+//                 setTimeout(() => {
+//                 document.body.style.backgroundColor = 'blue';
+//                 }, 1000);
+//             }, 1000);
+//     }, 1000);
+// }, 1000);
+
+// menggunakan promise untuk menghindari callback hell
+const gantiWarna = (color, delay) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-        document.body.style.backgroundColor = 'yellow';
-            setTimeout(() => {
-            document.body.style.backgroundColor = 'green';
-                setTimeout(() => {
-                document.body.style.backgroundColor = 'blue';
-                }, 1000);
-            }, 1000);
-    }, 1000);
-}, 1000);
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay);
+    })
+};
+
+gantiWarna('red', 1000)
+    .then(() => gantiWarna('yellow', 1000))
+    .then(() => gantiWarna('green', 1000))
+    .then(() => gantiWarna('blue', 1000));
