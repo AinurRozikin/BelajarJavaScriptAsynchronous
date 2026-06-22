dbb64c5c301048ad98b61352f1df5e8439eb892a
Ainur Rozikin <rozikinainur742@gmail.com>
2026-06-22 08:22:11 +0700

    menambahkan async await

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // set timeout sederhana dengan console log
-console.log('Kode ini berjalan sebelum set timeout');
-setTimeout(() => {
-    return console.log('Kode ini berjalan di dalam set timeout')
-    }, 10000);
-console.log('Kode ini berjalan setelah set timeout');
+// console.log('Kode ini berjalan sebelum set timeout');
+// setTimeout(() => {
+//     return console.log('Kode ini berjalan di dalam set timeout')
+//     }, 10000);
+// console.log('Kode ini berjalan setelah set timeout');
 
 // set timeout merubah warna halaman atau callback hell
 // setTimeout(() => {
@@ -20,7 +20,7 @@ console.log('Kode ini berjalan setelah set timeout');
 // }, 1000);
 
 // menggunakan promise untuk menghindari callback hell
-const gantiWarna = (color, delay) => {
+let gantiWarna = (color, delay) => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             document.body.style.backgroundColor = color;
@@ -29,7 +29,40 @@ const gantiWarna = (color, delay) => {
     })
 };
 
-gantiWarna('red', 1000)
-    .then(() => gantiWarna('yellow', 1000))
-    .then(() => gantiWarna('green', 1000))
-    .then(() => gantiWarna('blue', 1000));
+// gantiWarna('red', 1000)
+//     .then(() => gantiWarna('yellow', 1000))
+//     .then(() => gantiWarna('green', 1000))
+//     .then(() => gantiWarna('blue', 1000));
+
+
+
+// belajar javascript async 
+// const asinkronus = async() => {
+//     return 'Hello World';
+// }
+
+// asinkronus().then((res) => {
+//     console.log('response', res);
+// })
+
+const asinkronus = async() => {
+    throw 'Hello World';
+}
+
+asinkronus().then((res) => {
+    console.log('response', res);
+}).catch((err) => {
+    console.log('error', err);
+})
+
+async function jalankanPerubahan() {
+    await gantiWarna('red', 1000);
+    await gantiWarna('yellow', 1000);
+    await gantiWarna('green', 1000);
+    await gantiWarna('blue', 1000);
+    await gantiWarna('purple', 1000);
+    await gantiWarna('black', 1000);
+    return 'berjalan mulus';
+}
+
+jalankanPerubahan().then((res) => console.log(res));
